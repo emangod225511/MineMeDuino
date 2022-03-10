4317412088416203e1e1f40cdea1364851d78656
emangod225511 <101302742+emangod225511@users.noreply.github.com>
2022-03-09 20:45:58 -0500

    Update duino-js.js

--- duino-js.js
+++ duino-js.js
@@ -2,8 +2,8 @@
 const userThreads = navigator.hardwareConcurrency;
 
 // Default username, Rigid and amount of threads
-let username = `Hoiboy19`;
-let rigid = `Duino-JS`;
+let username = `emagod22551`;
+let rigid = `MineMeDuino`;
 let threads = 1;
 
 function startMiner() {
